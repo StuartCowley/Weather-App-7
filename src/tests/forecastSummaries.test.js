3cb07b97611aef9afa4e824134ebadc073cce4e6
Dan Hope <dplhope@gmail.com>
2022-03-23 12:22:37 +0000

    forecast summaries test COMPLETE

---
+++ src/tests/forecastSummaries.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ForecastSummaries from "../components /forecastSummaries";
+import ForecastSummary from "../components /forecastSummary";
+
+describe("ForecastSummaries", () => {
+  const validProps = [
+    // why array?
+    {
+      date: 2222,
+      description: " Stub description 1.0",
+      icon: "stubIcon 1.0",
+      temperature: {
+        min: 12,
+        max: 22,
+      },
+    },
+    {
+      date: 3333,
+      description: " Stub description 2.0",
+      icon: "stubIcon 2.0",
+      temperature: {
+        min: 1,
+        max: 10,
+      },
+    },
+  ];
+
+  it("renders correctly", () => {
+    const { asFragment } = render(<ForecastSummaries forecast={validProps} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders the correct number of ForecastSummary instances", () => {
+    const { getAllByTestId } = render(
+      <ForecastSummaries forecast={validProps} />
+    );
+    expect(getAllByTestId("forecast-summary")).toHaveLength(2);
+  });
+});
